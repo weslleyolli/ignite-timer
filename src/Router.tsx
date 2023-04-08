bd588dd15dbf40ca48e051aeb1a802ca27c32e4f
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-04-07 22:05:26 -0300

    Layout of routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/home'
 import { History } from './pages/history'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
