2d69de8c7dcee6499e4cbbe139af2a7257ab1d6d
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-04-09 23:19:51 -0300

    Header and layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,19 @@
+import { NavLink } from 'react-router-dom'
+import logoIgnite from '../assets/logo-ignite.svg'
+import { Timer, Scroll } from 'phosphor-react'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <div className="text-white font-bold text-3xl flex items-center justify-between">
+      <img src={logoIgnite} alt="" />
+      <nav className="flex gap-2">
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="History">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </div>
+  )
 }
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,7 +4,7 @@ import { Header } from '../components/Header'
 export function DefaultLayout() {
   return (
     <div className="w-1/2 h-screen m-auto border border-red-500 flex items-center ">
-      <div className="bg-red-500 h-3/4 w-full">
+      <div className="bg-gray800 h-3/4 w-full p-10 rounded-lg flex flex-col">
         <Header />
         <Outlet />
       </div>
