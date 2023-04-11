3bf43fd94d2888c9e66cb879c47febf1ca400bcc
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-04-11 19:27:28 -0300

    Home skeleton

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,32 @@
+import { Play } from 'phosphor-react'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="task">I will work in</label>
+          <input id="task" />
+
+          <label htmlFor="">for</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutes.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Start
+        </button>
+      </form>
+    </div>
+  )
 }
