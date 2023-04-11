599d10f8ef913ce2d88ac5230b3e642d5ef1f088
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-04-11 20:37:06 -0300

    form stiled

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,9 @@ import { Play } from 'phosphor-react'
 
 export function Home() {
   return (
-    <div>
-      <form action="">
-        <div>
+    <div className="flex-1 flex flex-col items-center justify-center">
+      <form action="" className="flex flex-col items-center gap-14">
+        <div className="w-full flex items-center justify-center gap-2 text-gray100 text-lg font-bold flex-wrap">
           <label htmlFor="task">I will work in</label>
           <input id="task" />
 
