880884c9b5fa3db9568a123b67b45029d641abe2
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-04-12 20:13:09 -0300

    page history

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,3 +1,52 @@
 export function History() {
-  return <h1>History page</h1>
+  return (
+    <div className="flex-1 p-14 flex flex-col">
+      <h1 className="text-2xl text-gray100 font-extrabold">My history</h1>
+
+      <div className="flex-1 overflow-auto mt-8">
+        <table className="w-full border-collapse ">
+          <thead>
+            <tr>
+              <th className="rounded-tl-lg pl-6">Task</th>
+              <th>During</th>
+              <th>Start</th>
+              <th className="rounded-tr-lg pr-6 ">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 min</td>
+              <td>2 years ago</td>
+              <td>Conclued</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 min</td>
+              <td>2 years ago</td>
+              <td>Conclued</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 min</td>
+              <td>2 years ago</td>
+              <td>Conclued</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 min</td>
+              <td>2 years ago</td>
+              <td>Conclued</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 min</td>
+              <td>2 years ago</td>
+              <td>Conclued</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
