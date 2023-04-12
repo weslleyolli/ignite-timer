f07d3ca5f9f85a95d02a567a4f335aab9ef4bd38
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-04-11 23:12:55 -0300

    page: home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,15 @@ export function Home() {
           <label htmlFor="">for</label>
           <input type="number" id="minutesAmount" />
 
-          <span>minutes.</span>
+          <p>minutes.</p>
         </div>
 
-        <div>
+        <div className="font-mono text-9xl leading-10 text-gray100 flex gap-4">
           <span>0</span>
           <span>0</span>
-          <span>:</span>
+          <span className="py-8 px-0 text-green500 w-16 overflow-hidden flex justify-center">
+            :
+          </span>
           <span>0</span>
           <span>0</span>
         </div>
