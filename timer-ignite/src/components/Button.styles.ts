fa05e8a34f82c0820169e4e8807070c1e99db106
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-04-18 15:31:28 -0300

    colors and fonts

--- timer-ignite/src/components/Button.styles.ts
+++ timer-ignite/src/components/Button.styles.ts
@@ -17,7 +17,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
 
-    ${props => {
-        return `color: ${buttonVariants[props.variant]}`
-    }}
+background-color: ${props => props.theme["green-500"]};
+color: ${props => props.theme.white};
 `
