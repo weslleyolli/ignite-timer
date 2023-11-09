62ab7938303d146e8ef1384e1456ee9c1dbd8a59
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-11-09 15:22:40 -0300

    Feat(global.ts): added global styles in project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
@@ -12,6 +13,7 @@ export function App() {
       <Button variant="secondary" />
       <Button variant="danger"/>
       <Button variant="success"/>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
